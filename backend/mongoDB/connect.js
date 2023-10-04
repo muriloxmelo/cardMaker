289540175560806ae5cb44af6e7bb4c161eb18df
murilo <muriloxmelo@gmail.com>
2023-10-04 18:19:04 -0300

    mongodb02

--- backend/mongoDB/connect.js
+++ backend/mongoDB/connect.js
@@ -4,7 +4,11 @@ require("dotenv").config();
 mongoose.set("strictQuery", true);
 
 async function connect() {
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0-cardmaker-7355.mpudbxj.mongodb.net/?retryWrites=true&w=majority`
+    )
+  );
   console.log("Database connected succesfully!");
 }
 
