396294bb565f21c8e2c671d273673e4c9d8c4519
murilo <muriloxmelo@gmail.com>
2023-10-04 11:49:41 -0300

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,13 @@ require("./mongoDB/connect.js"); // Conectar ao DB;
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://card-maker-beta.vercel.app/"],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
